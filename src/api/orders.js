9c03eca87d706c4b9492540bd6c6a9202782bedd
Hari Krishna <anemharikrishna@gmail.com>
2021-02-09 13:19:50 +0530

    Update orders.js

--- src/api/orders.js
+++ src/api/orders.js
@@ -1,13 +1,15 @@
 import axios from "axios";
 
+const baseAPIURL = "https://my-json-server.typicode.com/HariKrishna-9885699666/React-Mock-JSON-Data-Using-JSON-Server/orders";
+
 // Get all order
 export const getMyOrder = async () => {
-  const result = await axios.get("http://localhost:3001/orders");
+  const result = await axios.get(baseAPIURL);
   return result.data;
 };
 
 // Get order details by id
 export const getOrder = async (id) => {
-  const result = await axios.get("http://localhost:3001/orders/" + id);
+  const result = await axios.get(baseAPIURL + "/" + id);
   return result.data;
 };
